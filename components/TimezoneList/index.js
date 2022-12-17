79006495e65492f15fa9da9161c911b438dbda81
Wouter <wouter.van.marrum@protonmail.com>
2022-12-17 19:42:16 +0000

    feat: Enabled haptics on item press.
    This will give the user feedback that something has happened.

--- components/TimezoneList/index.js
+++ components/TimezoneList/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import moment from "moment-timezone";
 import {Button, Icon, List, ListItem} from "@ui-kitten/components";
 import * as Clipboard from 'expo-clipboard';
+import * as Haptics from 'expo-haptics';
 
 export default function ({timezones=[], onRemove}) {
   const icon = (
@@ -20,14 +21,19 @@ export default function ({timezones=[], onRemove}) {
   };
 
   const copyTime = async (item) => {
-    return await Clipboard.setStringAsync(moment().tz(item).format('HH:mm A'));
+    const copied = await Clipboard.setStringAsync(moment().tz(item).format('HH:mm A'));
+
+    if (copied) {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    }
   }
 
   const renderItem = ({ item, index }) => (
     <ListItem
       key={index}
       title={`${item} :: ${moment().tz(item).format('HH:mm A')}`}
-      onPressOut={() => copyTime(item)}
+      onPress={() => copyTime(item)}
       description={moment().tz(item).zoneAbbr()}
       accessoryLeft={icon}
       accessoryRight={() => removeButton(index)}
